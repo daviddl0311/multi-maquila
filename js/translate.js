dc614413193ab29c99ecdb78b0346950f3406599
daviddl0311 <daviddominguezladera@gmail.com>
2025-06-11 09:46:56 -0500

    Update Size, translator, width, img

--- js/translate.js
+++ js/translate.js
@@ -1,7 +1,7 @@
 function googleTranslateElementInit() {
 new google.translate.TranslateElement({
     pageLanguage: 'es',
-    includedLanguages: 'en,fr,it,pt,ru,zh-TW', // Añadido chino tradicional
+    includedLanguages: 'en,fr,de,it,pt,ru,zh-TW', // Añadido chino tradicional
     layout: google.translate.TranslateElement.InlineLayout.SIMPLE
 }, 'google_translate_element');
 }
